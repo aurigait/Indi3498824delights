88f3a3da41d858ef19ef8c851de82a6c0b2e9a25
sandeep jain <sandeepjain@aurigait.com>
2014-04-28 17:56:53 +0530

    Bug fixing voucher

--- app/code/local/Aurigait/Voucher/controllers/Checkout/CartController.php
+++ app/code/local/Aurigait/Voucher/controllers/Checkout/CartController.php
@@ -32,12 +32,14 @@ class Aurigait_Voucher_Checkout_CartController extends Mage_Checkout_CartControl
 			
 			// sandeep::  to add condtions for validation. 
 			
-			if(!$this->checkcustomcondition($couponCode))
+			$retrun  = $this->checkcustomcondition($couponCode);
+			
+			if($retrun==-1)
 			{
 				$this->_goBack();
 				return;
 			}
-		
+			
             $codeLength = strlen($couponCode);
             $isCodeLengthValid = $codeLength && $codeLength <= Mage_Checkout_Helper_Cart::COUPON_CODE_MAX_LENGTH;
 
@@ -45,7 +47,7 @@ class Aurigait_Voucher_Checkout_CartController extends Mage_Checkout_CartControl
             $this->_getQuote()->setCouponCode($isCodeLengthValid ? $couponCode : '')
                 ->collectTotals()
                 ->save();
-
+			
             if ($codeLength) {
                 if ($isCodeLengthValid && $couponCode == $this->_getQuote()->getCouponCode()) {
                     $this->_getSession()->addSuccess(
@@ -92,36 +94,38 @@ class Aurigait_Voucher_Checkout_CartController extends Mage_Checkout_CartControl
 						
 					$daygap = ($configValue * 24 * 60 * 60);
 					$registerdate = $customer->getCreatedAtTimestamp();
-						
+					
 					$totaldayspassed =  $customer->getCreatedAtTimestamp() + $daygap;
-						
-					if( strtotime(now()) < $totaldayspassed)
+					
+					if( strtotime(now()) > $totaldayspassed)
 					{
 						//echo 123;die;
 						$this->_getSession()->addError(
-								$this->__('Coupon code "%s" is available for  %s  after registeration', Mage::helper('core')->escapeHtml($couponCode) ,$configValue)
+								$this->__('Coupon code "%s" is available for  %s days  after registeration.', Mage::helper('core')->escapeHtml($couponcode) ,$configValue)
 						);
 						$this->_goBack();
-						return;
+						return -1;
 					}
+					else
+						return 1;
 				}
 				else
 				{
 					// has already  placed an order
 					$this->_getSession()->addError(
-							$this->__('Coupon code "%s" only for First purchase .', Mage::helper('core')->escapeHtml($couponCode))
+							$this->__('Coupon code "%s" is available only for First purchase.', Mage::helper('core')->escapeHtml($couponcode))
 					);
 					$this->_goBack();
-					return;
+					return -1;
 				}
 			}
 			else
 			{
 				$this->_getSession()->addError(
-						$this->__('Use Coupon code "%s" after login .', Mage::helper('core')->escapeHtml($couponCode))
+						$this->__('Use Coupon code "%s" after login.', Mage::helper('core')->escapeHtml($couponcode))
 				);
 				$this->_goBack();
-				return;
+				return -1;
 			}
 		}
 		else if(substr($couponcode,0,4) == Aurigait_Voucher_IndexController::CUSTCODE)
@@ -133,11 +137,13 @@ class Aurigait_Voucher_Checkout_CartController extends Mage_Checkout_CartControl
 				if(!$isValiduser)
 				{
 					$this->_getSession()->addError(
-							$this->__('You are not valid to use  "%s" coupon .', Mage::helper('core')->escapeHtml($couponCode))
+							$this->__('You are not valid person to use  "%s" coupon .', Mage::helper('core')->escapeHtml($couponCode))
 					);
 					$this->_goBack();
-					return;
+					return -1;
 				}
+				else
+					return true;
 			}
 			else
 			{
@@ -145,7 +151,7 @@ class Aurigait_Voucher_Checkout_CartController extends Mage_Checkout_CartControl
 						$this->__('Use Coupon code "%s" after login .', Mage::helper('core')->escapeHtml($couponCode))
 				);
 				$this->_goBack();
-				return;
+				return -1;
 			}
 						 					
 		}
