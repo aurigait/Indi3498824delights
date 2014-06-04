348be2273d4a3c86a4f52f8137eb7ff313886252
somesh <somesh.soni@aurigait.com>
2014-06-04 19:56:15 +0530

    Landing popup login issue resolved

--- app/code/local/Aurigait/Landingpage/controllers/RegisteruserController.php
+++ app/code/local/Aurigait/Landingpage/controllers/RegisteruserController.php
@@ -87,10 +87,6 @@ class Aurigait_Landingpage_RegisteruserController extends Mage_Customer_AccountC
 		$errors = array();
 		/* @var $address Mage_Customer_Model_Address */
 		$address = Mage::getModel('customer/address');
-		
-		
-		
-		
 		/* @var $addressForm Mage_Customer_Model_Form */
 		$addressData = $addressForm->extractData($this->getRequest(), 'address', false);
 		$addressErrors = $addressForm->validateData($addressData);
@@ -134,5 +130,101 @@ class Aurigait_Landingpage_RegisteruserController extends Mage_Customer_AccountC
 		$this->_redirectSuccess(Mage::getUrl('customer/account/'));
 		return $this;
 	}
+	public function loginPostAction()
+	{
+		if (!$this->_validateFormKey()) {
+			$this->_redirect('*/*/');
+			return;
+		}
+	
+		if ($this->_getSession()->isLoggedIn()) {
+			$this->_redirect('*/*/');
+			return;
+		}
+		$session = $this->_getSession();
+	
+		if ($this->getRequest()->isPost()) {
+			$login = $this->getRequest()->getPost('login');
+			if (!empty($login['username']) && !empty($login['password'])) {
+				try {
+					$session->login($login['username'], $login['password']);
+					if ($session->getCustomer()->getIsJustConfirmed()) {
+						$this->_welcomeCustomer($session->getCustomer(), true);
+					}
+				} catch (Mage_Core_Exception $e) {
+					switch ($e->getCode()) {
+						case Mage_Customer_Model_Customer::EXCEPTION_EMAIL_NOT_CONFIRMED:
+							$value = $this->_getHelper('customer')->getEmailConfirmationUrl($login['username']);
+							$message = $this->_getHelper('customer')->__('This account is not confirmed. <a href="%s">Click here</a> to resend confirmation email.', $value);
+							break;
+						case Mage_Customer_Model_Customer::EXCEPTION_INVALID_EMAIL_OR_PASSWORD:
+							$message = $e->getMessage();
+							break;
+						default:
+							$message = $e->getMessage();
+					}
+					$session->addError($message);
+					$session->setUsername($login['username']);
+				} catch (Exception $e) {
+					// Mage::logException($e); // PA DSS violation: this exception log can disclose customer password
+				}
+			} else {
+				$session->addError($this->__('Login and password are required.'));
+			}
+		}
+	
+		$this->_loginPostRedirect();
+	}
+	
+	/**
+	 * Define target URL and redirect customer after logging in
+	 */
+	protected function _loginPostRedirect()
+	{
+		$session = $this->_getSession();
+	
+		if (!$session->getBeforeAuthUrl() || $session->getBeforeAuthUrl() == Mage::getBaseUrl()) {
+			// Set default URL to redirect customer to
+			$session->setBeforeAuthUrl($this->_getHelper('customer')->getAccountUrl());
+			// Redirect customer to the last page visited after logging in
+			if ($session->isLoggedIn()) {
+				if (!Mage::getStoreConfigFlag(
+						Mage_Customer_Helper_Data::XML_PATH_CUSTOMER_STARTUP_REDIRECT_TO_DASHBOARD
+				)) {
+					$referer = $this->getRequest()->getParam(Mage_Customer_Helper_Data::REFERER_QUERY_PARAM_NAME);
+					if ($referer) {
+						// Rebuild referer URL to handle the case when SID was changed
+						$referer = $this->_getModel('core/url')
+						->getRebuiltUrl( $this->_getHelper('core')->urlDecode($referer));
+						if ($this->_isUrlInternal($referer)) {
+							$session->setBeforeAuthUrl($referer);
+						}
+					}
+				} else if ($session->getAfterAuthUrl()) {
+					$session->setBeforeAuthUrl($session->getAfterAuthUrl(true));
+				}
+			} else {
+				$session->setBeforeAuthUrl( $this->_getHelper('customer')->getLoginUrl());
+			}
+		} else if ($session->getBeforeAuthUrl() ==  $this->_getHelper('customer')->getLogoutUrl()) {
+			$session->setBeforeAuthUrl( $this->_getHelper('customer')->getDashboardUrl());
+		} else {
+			if (!$session->getAfterAuthUrl()) {
+				$session->setAfterAuthUrl($session->getBeforeAuthUrl());
+			}
+			if ($session->isLoggedIn()) {
+				$session->setBeforeAuthUrl($session->getAfterAuthUrl(true));
+			}
+		}
+		$post=Mage::app()->getRequest()->getPost();
+		if($post['referers'])
+		{
+			//echo Mage::helper('core')->urlDecode($post['referers']);die;
+			$this->_redirectUrl(Mage::helper('core')->urlDecode($post['referers']));
+		}
+		else 
+			$this->_redirectUrl($session->getBeforeAuthUrl(true));
+	}
+	
 }
 ?>
